54fddc57b5550c0dc196dfdc008b29df09b94dcd
allyanna <u1223264@eng.utah.edu>
2022-02-17 02:43:23 -0700

    worked with drawing panel (grid panel) to be added to toolbox later and added notes on what one user data will look like

--- GridPanels/GridPanel.cs
+++ GridPanels/GridPanel.cs
@@ -8,13 +8,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace GridPanels
 {
     public partial class GridPanel : UserControl
     {
+        //Draws the panel grid for each pixel
+/*        private DrawingPanel drawingPanel;*/
+
         public GridPanel()
         {
             InitializeComponent();
+
+            //initialize our drawing panel
+           /* drawingPanel = new DrawingPanel(this);
+            drawingPanel.Location = new Point(0, 0); //start here
+
+            Controls.Add(drawingPanel); //make drawing panel a part of the grid panel*/
         }
     }
 }
--- Model/UserData.cs
+++ Model/UserData.cs
@@ -6,6 +6,7 @@ namespace Model
     {
 
         //store the dates, corresponding data to that user 
-
+        //TODO: what types of entry do we want to have for one user?
+        //TODO: what sorts of structure are we looking for to store?
     }
 }
